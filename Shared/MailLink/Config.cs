896439bbc54d03cb215afc2757d7e0068b3c18d2
David McCartney <dmccartney@coreslab.com>
2017-07-20 09:09:37 -0500

    Initial Commit

--- Shared/MailLink/Config.cs
+++ Shared/MailLink/Config.cs
@@ -9,73 +9,204 @@ namespace MailLink
     public class Config
     {
 
-        private Configuration configuration;
+        public Configuration Root { get; protected set; }
 
         public Config()
         {
-            if (!File.Exists(@"config.xml")) 
+            if (!File.Exists(@"c:\projects\config.xml")) 
             {
                 Create();
                 Save();
             }
             else
             {
-                Load();
+                Create(); //Temporarly skip loading
+                Save();
+                //Load();
             }
         }
 
         private void Create()
         {
-            configuration = new Configuration();
+            Root = new Configuration();
 
+            Root.PollInterval   =  5 * 60; // Five Minutes
+            Root.SystemInterval = 15 * 60; // Fifteen Minutes
+
+            Root.Settings.Add(new Setting("Testing", "Test"));
+
+            Root.Servers.Add(new Server("SMTP", "CSSOK", "cssokc-ms1.coreslab.local", 25));
+            Root.Servers.Add(new Server("POP", "Coreslab", "psmtp.coreslab.com", 110));
+            Root.Servers.Add(new Server("POP", "IT1", "pop.it1.biz", 110));
+
+            Root.Users.Add(new User("David McCartney","dmc@it1.biz","IT1","CSSOK","dmccartney","Maui2010!"));
+            Root.Users.Add(new User { Name = "David McCartney", Email = "dmccartney@coreslab.com",
+                                               Inbound = { Server = "Coreslab", Username = "dmccartney", Password = "Maui2010!" },
+                                               Outbound = { Server = "CSSOK" }  });
         }
 
         public void Save()
         {
-            FileStream fs = new FileStream(@"config.xml", FileMode.Create);
+            FileStream fs = new FileStream(@"c:\projects\config.xml", FileMode.Create);
 
+            XmlSerializerNamespaces xmlns = new XmlSerializerNamespaces();
+            //xmlns.Add("ML", "https://github.com/David-McCartney/MailLink");
+            xmlns.Add("", "");
+            XmlSerializer xml = new XmlSerializer(typeof(Configuration));
 
+            xml.Serialize(fs, Root, xmlns);
+            //xml.Serialize(fs, configuration);
+
+            fs.Close();
         }
 
         public void Load()
         {
-            FileStream fs = new FileStream(@"config.xml", FileMode.Open);
+            FileStream fs = new FileStream(@"c:\projects\config.xml", FileMode.Open);
 
-            XmlSerializer xml = new XmlSerializer(typeof(List<Setting>), new XmlRootAttribute("Configuration"));
-            try
-            {
-                configuration = (Configuration)xml.Deserialize(fs);
-            }
-            catch { }
+            XmlSerializer xml = new XmlSerializer(typeof(Configuration));
+            Root = (Configuration)xml.Deserialize(fs);
+
+            fs.Close();
         }
 
     }
 
-
-    [XmlRootAttribute("Configuration", Namespace = "http://www.cpandl.com", IsNullable = false)]]
+    [XmlRoot]
     public class Configuration
     {
+        [XmlElement]
+        public int PollInterval { get; set; }
 
+        [XmlElement]
+        public int SystemInterval { get; set; }
+
+        [XmlArrayItem(ElementName = "Setting", Type = typeof(Setting))]
         public List<Setting> Settings { get; set; }
 
+        [XmlArrayItem(ElementName = "Server", Type = typeof(Server))]
+        public List<Server> Servers { get; set; }
 
+        [XmlArrayItem(ElementName = "User", Type = typeof(User))]
+        public List<User> Users { get; set; }
+
+        public Configuration()
+        {
+            Settings = new List<Setting>();
+            Servers = new List<Server>();
+            Users = new List<User>();
+        }
     }
-
 
     public class Setting
     {
+        [XmlAttribute]
         public string Name { get; set; }
+
+        [XmlText]
         public string Value { get; set; }
 
-        Setting()
+        public Setting()
         {
 
         }
 
-        Setting(String name, string value)
+        public Setting(String name, string value)
         {
             Name = name;
             Value = value;
+        }
+    }
+
+    public class Server
+    {
+        [XmlAttribute]
+        public string Type { get; set; }
+
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        [XmlElement]
+        public string Domain { get; set; }
+
+        [XmlElement]
+        public int Port { get; set; }
+
+        [XmlElement]
+        public bool RequireSSL { get; set; }
+
+        public Server()
+        {
+
+        }
+
+        public Server(string type, string name, string domain, int port)
+        {
+            Type = type;
+            Name = name;
+            Domain = domain;
+            Port = port;
+
+            RequireSSL = false;
+        }
+    }
+
+    public class User
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        [XmlElement]
+        public string Email { get; set; }
+
+        [XmlElement]
+        public Credentials Inbound { get; set; }
+
+        [XmlElement]
+        public Credentials Outbound { get; set; }
+
+        [XmlIgnore]
+        public bool Busy { get; set; }
+
+        public User()
+        {
+            Inbound = new Credentials();
+            Outbound = new Credentials();
+        }
+
+        public User(string name, string email, string inbound, string outbound, string username, string pasword)
+        {
+            Name = name;
+            Email = email;
+            Inbound = new Credentials(inbound, username, pasword);
+            Outbound = new Credentials(outbound, null, null);
+
+            Busy = false;
+        }
+    }
+
+    public class Credentials
+    {
+        [XmlAttribute]
+        public string Server { get; set; }
+
+        [XmlElement]
+        public string Username { get; set; }
+
+        //TODO: Encript Password
+        [XmlElement]
+        public string Password { get; set; }
+
+        public Credentials()
+        {
+
+        }
+
+        public Credentials(string server, string username, string password)
+        {
+            Server = server;
+            Username = username;
+            Password = password;
         }
     }
 }
--- Shared/MailLink/Connector.cs
+++ Shared/MailLink/Connector.cs
@@ -1,42 +1,38 @@
-﻿using System;
+﻿using MailKit.Net.Pop3;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MailLink
 {
     public class Connector
     {
+        System.Timers.Timer tick;
+        System.Timers.Timer tock;
+
         const int DEFAULT_SMTP_PORT = 25;
         const int DEFAULT_POP3_PORT = 110;
+
+        private Config config;
 
         public bool VerboseLogging { get; set; }
 
         public string Message { get; protected set; }
 
         /// <summary>
-        /// Occurs when a new client has connected to the server.
+        /// Occurs when a message needs to be written to the event log.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         public event EventHandler<EventArgs> LogMessage = delegate { };
 
-        /// <summary>
-        /// Specifies the IP Address to bind for the TCP listener.
-        /// </summary>
-        public IPAddress ServerIP { private get; set; }
-
-        /// <summary>
-        /// Specifies the Port to bind for the TCP listener.
-        /// </summary>
-        public int ServerPort { private get; set; }
-
         private bool active;
-        private static TcpListener tcpListener;
 
         /// <summary>
         /// TODO: Comment
@@ -44,11 +40,81 @@ namespace MailLink
         public Connector()
         {
             active = false;
-            ServerIP = IPAddress.Any;
-            ServerPort = DEFAULT_SMTP_PORT;
 
-            VerboseLogging = false;
+            VerboseLogging = true;
 
+            config = new Config();
+
+            // Inilize polling clock, used for retrieving email from POP and IMAP servers.
+            tick = new System.Timers.Timer();
+            //tick.Interval = config.Root.PollInterval * 1000;
+            tick.Interval = 5000;
+            tick.Elapsed += onTickElapsed;
+
+            // Initialize system clock, used for sending ActivityLogs, ErrorLogs, and Notifications to mail administrators.
+            tock = new System.Timers.Timer();
+            tock.Interval = config.Root.SystemInterval * 1000;
+            //tock.AutoReset = true;
+            tock.Elapsed += onTockElapsed;
+        }
+
+        private void onTickElapsed(object sender, System.Timers.ElapsedEventArgs ea)
+        {
+            if (!active) return;
+
+            foreach(User user in config.Root.Users)
+            {
+                if(!user.Busy)
+                {
+                    user.Busy = true;
+
+                    new Thread(() =>
+                    {
+                        Thread.CurrentThread.IsBackground = true;
+
+                        using (var client = new Pop3Client())
+                        {
+                            client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                            Server server = config.Root.Servers.Where(s => s.Name == user.Inbound.Server).First();
+
+
+                            try
+                            {
+                                client.Connect(server.Domain, server.Port, false);
+
+                                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                                client.Authenticate(user.Inbound.Password, user.Inbound.Password);
+
+                                //for (int i = 0; i < client.Count; i++)
+                                //{
+                                //    var message = client.GetMessage(i);
+                                //    Console.WriteLine("Subject: {0}", message.Subject);
+                                //}
+                                Console.WriteLine("User: {0} has {1} messges.\r\n", user.Name, client.Count());
+
+                                client.Disconnect(true);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Error connectiong to {0} : {1}\r\n", user.Inbound.Server, e.Message);
+
+                                if (client != null) client.Dispose();
+                            }
+                        }
+
+                        user.Busy = false;
+
+                    }).Start();
+                }
+            }
+        }
+
+        private void onTockElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (!active) return;
+
+            // TODO: Email Notifications
         }
 
         private void WriteLog(string message)
@@ -57,7 +123,7 @@ namespace MailLink
             LogMessage(this, new EventArgs());
         }
 
-        public async void Start()
+        public void Start()
         {
             if (active == true) return;
             else active = true;
@@ -66,25 +132,42 @@ namespace MailLink
 
             WriteLog("Initializing...");
 
-            WriteLog("\rReady to receive connections. Type 'HELP' for a list of commands.\n");
+            
 
+
+            WriteLog("\rInitialization Complete. Type 'HELP' for a list of commands.\n\n");
+
+            tick.Start();
+            tock.Start();
 
         }
 
 
         public void Pause()
         {
-            tcpListener.Stop();
+            active = false;
+
+            tick.Stop();
+            tock.Stop();
+
         }
 
         public void Resume()
         {
-            tcpListener.Start();
+            active = true;
+
+            tick.Start();
+            tock.Start();
         }
 
         public void Stop()
         {
             active = false;
+
+            tick.Stop();
+            tock.Stop();
+
+            //TODO: Disconnect and dispose all active connections.
         }
     }
 }
